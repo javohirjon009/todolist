3a2928ffd148d790e4caca8ccb06569ac6d46381
javohirjon009 <javohirakramov231@gmail.com>
2025-04-29 16:49:41 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import React, { useRef} from "react";
+import React from "react";
 
 const App = () => {
-  // const [state, setSate] = useState("");
-
-  // let ref = useRef(null);
-  // const click = () => {
-  //   if (ref.current) {
-  //     ref.current.style.backgroundColor = "red";
-  //   }
-  // };
-  return <div>
-
-        .todo-item { display: flex; justify-content: space-between; margin: 5px 0; }
-
-    <h2>TodoList</h2>
-    <input type="text" id="todoInput" placeholder="Yangi vazifa">
-    <button onclick="addTodo()">Qo'shish</button>
-    <ul id="todoList"></ul>
-    <script src="script.js"></script>
-
-  </div>;
+  return (
+    <div>
+      <h2>TodoList</h2>
+      <input type="text" id="todoInput" placeholder="Yangi vazifa" />
+      <button onclick="addTodo()">Qo'shish</button>
+      <ul id="todoList"></ul>
+      <script src="script.js"></script>
+    </div>
+  );
 };
 
 export default App;
